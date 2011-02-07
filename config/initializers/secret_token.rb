03e6d0daa2dd3178d1d3078476761ffd7a72d555
jeff <jeffshea206@gmail.com>
2011-02-06 21:30:54 -0800

    First commit of Rails project Tickets from the Head First Rails book

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets::Application.config.secret_token = '8f21e50c96d41727b700874ae6a8538dd0dd84b2e39cff067658f302cd55f1b8af13e156548258e1e0d6c2bd44776c2a41b3e31f3f1c633ebac6c54d722aa6bb'
